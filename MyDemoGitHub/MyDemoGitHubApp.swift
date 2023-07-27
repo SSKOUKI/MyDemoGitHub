841eb9223939d1f6917251a696df55879ce468ce
Samia <koukii.samia@gmail.com>
2023-07-27 12:07:26 +0400

    Initial Commit

---
+++ MyDemoGitHub/MyDemoGitHubApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyDemoGitHubApp.swift
+//  MyDemoGitHub
+//
+//  Created by APPLE on 7/27/23.
+//
+
+import SwiftUI
+
+@main
+struct MyDemoGitHubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
