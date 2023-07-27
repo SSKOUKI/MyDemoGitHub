af65f19fbb6e56efb924cb9e2d905d35cd64235a
Samia <koukii.samia@gmail.com>
2023-07-27 12:11:03 +0400

    This is a new message

--- MyDemoGitHub/ContentView.swift
+++ MyDemoGitHub/ContentView.swift
@@ -14,6 +14,7 @@ struct ContentView: View {
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            Text("This is to show how to commit chnages in Github")
         }
         .padding()
     }
